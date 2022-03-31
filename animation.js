682319c5c6824fd20bc5d3550c9ae7116fa037db
lhs-ktt-32922 <102624635+lhs-ktt-32922@users.noreply.github.com>
2022-03-31 08:46:43 -0700

    Move javascript to separate file

---
+++ animation.js
@@ -0,0 +1,49 @@
+let canvas = document.getElementById("canvas");
+
+let WIDTH  = canvas.getAttribute("width");
+let HEIGHT = canvas.getAttribute("height");
+
+let ctx = canvas.getContext("2d");
+
+let debugEl = document.getElementById("debug");
+function debug(txt) {
+	debugEl.innerText = txt;
+}
+
+// triangle wave function, outputs 0 to 1
+let tWave=x=>Math.abs(Math.abs(x) % 2 - 1);
+
+let pos = [Math.random(), Math.random()];
+let theta = Math.random() * Math.PI * 2;
+let dir = [Math.cos(theta), Math.sin(theta)];
+
+let speed = 75;
+
+ctx.textBaseline = "top";
+ctx.font = "2em Arial";
+let txt = "Hello";
+let txm = ctx.measureText(txt);
+let txtWidth = txm.width;
+let txtHeight = txm.fontBoundingBoxDescent;
+
+let prevTime = 0.0;
+let t = 0.0;
+
+const FPS = 60;
+
+function animation() {
+
+	ctx.clearRect(0, 0, WIDTH, HEIGHT);
+
+	let deltaTime = t - prevTime;
+
+	let x = tWave(pos[0] + t * deltaTime * dir[0] * speed) * (WIDTH - txtWidth);
+	let y = tWave(pos[1] + t * deltaTime * dir[1] * speed) * (HEIGHT - txtHeight);
+
+	ctx.fillText(txt, x, y);
+
+	prevTime = t;
+	t += 1/60;
+}
+
+setInterval(animation, 1000/FPS);
